36e68b6482fb1e3ba1d783f106a8d54631b70a0d
Roman Brito <Roman Brito>
2017-05-09 14:10:31 -0500

    another try

--- controllers/api_flight.js
+++ controllers/api_flight.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var http = require('http');
-var https = require('https');
+var request = require("request");
 var db = require("../models");
 var config = require('../config/config.json');
 var dateFormat = require('dateformat');
@@ -24,32 +23,82 @@ function FlightQueryURL(APIname, protocol, version, format, parameters, options)
     return queryURL;
 }
 
-function flightObject (dbFlight, cb) {
-    var apFlight = [];
-    for (var i = 0; i < dbFlight.length; i++) {
-        var flightDate =  dbFlight[i].flight_date;
-        var flightNumber =  dbFlight[i].flight_number;
-        var flightNumberArrStr = flightNumber.split(" ");
-        var airlineCode = flightNumberArrStr[0];
-        var flight = flightNumberArrStr[1];
-        flightDate = dateFormat(flightDate, "yyyy/m/d");
+// function flightObject
+
+// function flightObject (dbFlight, cb) {
+//     var apFlight = [];
+//     for (var i = 0; i < dbFlight.length; i++) {
+//         var flightDate =  dbFlight[i].flight_date;
+//         var flightNumber =  dbFlight[i].flight_number;
+//         var flightNumberArrStr = flightNumber.split(" ");
+//         var airlineCode = flightNumberArrStr[0];
+//         var flight = flightNumberArrStr[1];
+//         flightDate = dateFormat(flightDate, "yyyy/m/d");
+//
+//
+//         var parameters = "flight/status/" + airlineCode + "/" + flight + "/arr/" + flightDate;
+//         var url = FlightQueryURL("flightstatus", "rest", "v2", "json", parameters, "", "flightInfo");
+//
+//         var request = https.get(url, function (response) {
+//             var buffer = ""
+//                 ,data;
+//             response.on("data", function (chunk) {
+//                 buffer += chunk;
+//             });
+//             response.on("end", function (err) {
+//                 apFlight.push(JSON.parse(buffer));
+//             });
+//         });
+//     }
+//     cb(apFlight);
+// }
+
+function apiObject (counter, flightInfo, dbFlight, i, cb)
+{
+    console.log("the index is " + i);
+    console.log("the counter is " + counter);
+
+    var flightDate =  dbFlight[i].flight_date;
+    var flightNumber =  dbFlight[i].flight_number;
+    var flightNumberArrStr = flightNumber.split(" ");
+    var airlineCode = flightNumberArrStr[0];
+    var flight = flightNumberArrStr[1];
+    flightDate = dateFormat(flightDate, "yyyy/m/d");
 
 
-        var parameters = "flight/status/" + airlineCode + "/" + flight + "/arr/" + flightDate;
-        var url = FlightQueryURL("flightstatus", "rest", "v2", "json", parameters, "", "flightInfo");
+    var parameters = "flight/status/" + airlineCode + "/" + flight + "/arr/" + flightDate;
+    var url = FlightQueryURL("flightstatus", "rest", "v2", "json", parameters, "", "flightInfo");
 
-        var request = https.get(url, function (response) {
-            var buffer = ""
-                ,data;
-            response.on("data", function (chunk) {
-                buffer += chunk;
-            });
-            response.on("end", function (err) {
-                apFlight.push(JSON.parse(buffer));
-            });
+    // var request = https.get(url, function (response) {
+    //     var buffer = ""
+    //         ,data;
+    //     response.on("data", function (chunk) {
+    //         buffer += chunk;
+    //     });
+    //     response.on("end", function (err) {
+    //         flightInfo.push(JSON.parse(buffer));
+    //     });
+    // });
+    if(counter > 1) {
+        request(url, function (error, response, body) {
+
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
+
+                flightInfo.push(JSON.parse(body));
+
+
+                // console.log("counter " + counter);
+                console.log(flightInfo);
+                return cb(flightInfo);
+
+            }
         });
+    } else {
+        // console.log("this is the request var " + request);
+        return apiObject(counter-1, flightInfo, dbFlight, i+1);
     }
-    cb(apFlight);
+
 }
 
 router.post("/flights", function (req, res) {
@@ -72,6 +121,14 @@ router.get("/flights", function (req, res) {
             TripId: TripId
         }
     }).then(function (dbFlight) {
+        apiObject(dbFlight.length,[], dbFlight,0, function (data) {
+            var hbsObject = {
+                flights: dbFlight,
+                flighStatus: data
+            };
+            res.json(hbsObject);
+            // res.render("flight", hbsObject);
+        });
 
         // flight api
 
@@ -108,16 +165,12 @@ router.get("/flights", function (req, res) {
         //         // res.render("flight", hbsObject);
         //     });
         // });
+        //
+        //     var hbsObject = {
+        //         flights: dbFlight,
+        //         flightStatus: apiObject(dbFlight.length-1,[], dbFlight,0)
+        //     };
 
-        flightObject(dbFlight, function (apiObject) {
-            var hbsObject = {
-                flights: dbFlight,
-                flightStatus: apiObject
-            };
-            res.json(hbsObject);
-            // res.render("flight", hbsObject);
-
-        });
     });
 });
 
